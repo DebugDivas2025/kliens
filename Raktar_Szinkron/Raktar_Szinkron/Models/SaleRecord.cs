402246b803fda2255cad4a5b04c3c198eb4c4063
DebugDivas2025 <zsofia.zsambokrety@stud.uni-corvinus.hu>
2025-04-23 09:03:09 +0200

    Alapstuktúra fájlok létrehozva

---
+++ Raktar_Szinkron/Raktar_Szinkron/Models/SaleRecord.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raktar_Szinkron.Models
+{
+    public class SaleRecord
+    {
+        public string SKU { get; set; }
+        public int Quantity { get; set; }
+        public DateTime SoldAt { get; set; }
+        public bool Synced { get; set; }
+    }
+}
